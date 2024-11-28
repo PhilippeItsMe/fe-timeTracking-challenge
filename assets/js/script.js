24ef3c80095f9da1c5596dc53c0e8405792ce473
Philippe Tschanun <philippe@digitalgrowthagency.ch>
2024-11-28 15:53:03 +0000

    Extraction key data form the DOM and setting up the cards colors

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+//Extracting DOM infos and setting up the colors array
+const btns = document.querySelectorAll('.button');
+const activityBoxes = document.querySlectro('.activity-box');
+const backgroundColors = [
+    '#FF8B64',
+    '#55C2E6',
+    '#FF5E7D',
+    '#4BCF82',
+    '#7335D2',
+    '#F1C75B'
+]
